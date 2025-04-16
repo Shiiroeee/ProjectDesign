1d706969ddbb5d2ccfda9099c39fb37917ea0085
Shiiiroeee <118511624+Shiiroeee@users.noreply.github.com>
2025-04-16 14:21:14 +0800

    Update

---
+++ main.js
@@ -0,0 +1,18 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true,
+    }
+  });
+
+  win.loadFile('frontend/build/index.html');
+}
+
+app.whenReady().then(createWindow);
